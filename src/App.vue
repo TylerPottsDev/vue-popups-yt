dcaffa0ecc17857049f61bdf5552fe9fce8f3b71
Tyler Potts <tylerpottsdev@gmail.com>
2021-01-13 15:02:10 +0000

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<main>
+		<h1>Vue Popups</h1>
+		<button @click="() => TogglePopup('buttonTrigger')">Open Popup</button>
+		
+		<Popup 
+			v-if="popupTriggers.buttonTrigger" 
+			:TogglePopup="() => TogglePopup('buttonTrigger')">
+			<h2>My Button Popup</h2>
+		</Popup>
+		<Popup 
+			v-if="popupTriggers.timedTrigger"
+			:TogglePopup="() => TogglePopup('timedTrigger')">
+			<h2>My Timed Popup</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, accusantium sequi. Libero velit assumenda odio dolor repellendus earum debitis culpa voluptatum, illum beatae? Quasi, modi omnis repellat adipisci voluptate assumenda!
+			</p>
+		</Popup>
+	</main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+import Popup from './components/Popup';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	setup () {
+		const popupTriggers = ref({
+			buttonTrigger: false,
+			timedTrigger: false
+		});
+
+		const TogglePopup = (trigger) => {
+			popupTriggers.value[trigger] = !popupTriggers.value[trigger]
+		}
+
+		setTimeout(() => {
+			popupTriggers.value.timedTrigger = true;
+		}, 3000);
+
+		return {
+			Popup,
+			popupTriggers,
+			TogglePopup
+		}
+	}
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
